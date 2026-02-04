6bdbf5df059f518a6dc36b6af1dfb6b3c2572839
phzineasygame <pedroflorencio@colegiopaulodetarso.com.br>
2026-02-04 16:03:46 +0000

    primeira aula c

---
+++ comentário.c
@@ -0,0 +1,7 @@
+o #include <stdio.h> é um comando que importa a biblioteca padrão de input e output da linguagem C, para usar funções como o print.
+
+o programa começa na função main
+
+tirar o return 0 pode trazer erros de compilação do código
+
+se tirar o /n, o texto vai continuar na mesma linha, não vai pular outra linha, se mantendo na mesma.
